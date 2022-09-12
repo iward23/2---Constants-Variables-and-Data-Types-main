df3f592ccf97aec7728cb482f80fdf67429106fc
Imani Ward <idward712@gmail.com>
2022-09-12 11:05:47 -0400

    o

--- Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,17 +3,18 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking the variable name.
  */
-
+var firstDecimal = 1.3
+var secondDecimal = 2.3
 
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var trueOrFalse = true
+print("This will not compile because you cannot assign a boolean to a double")
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var word = "the"
+print("This will not compile because you cannot assign a string to a double")
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var wholeNumber = 2
+print("This code will not compile because you cannot assign an integer to a double")
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
  */
--- Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
@@ -3,16 +3,18 @@
  
  Declare a variable called `name` of type `String`, but do not give it a value. Print `name` to the console. Does the code compile? Remove any code that will not compile.
  */
-
-
+var name = ""
+print(name)
+print("The code does compile because it is an empty string that is being printed")
 //:  Now assign a value to `name`, and print it to the console.
-
-
+name = "Jake"
+print(name)
 //:  Declare a variable called `distanceTraveled` and set it to 0. Do not give it an explicit type.
-
+var distanceTraveled = 0
 
 //:  Now assign a value of 54.3 to `distanceTraveled`. Does the code compile? Go back and set an explicit type on `distanceTraveled` so the code will compile.
 
+print("The code does not compile because you cannot assign a ")
 
 /*:
 [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Percent Completed](@next)
